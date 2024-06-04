f33aa57e76b856fe73ef6945568f3f1eb55d0169
FrancoCos <francotomascos@gmail.com>
2024-06-03 21:48:18 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './components/Navbar'
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Contact from './pages/Contact';
 function App() {
 
   return (
-    <>
-    <NavBar/>
-    <h1>En proceso...</h1>
-    </>
-  )
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <NavBar />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App
